491a5b4352595240d17d91beed54f55ed9471bb8
Omkarbade <omakrbade3799@gmail.com>
2022-02-07 23:27:06 +0530

    [Omkar] Add Inventorydata program

---
+++ OOPS/InventoryManagement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    public class InventoryManagement
+    {
+
+        public void ReadData(string filepath)
+        {
+            try
+            {
+                using (System.IO.StreamReader reader = new System.IO.StreamReader(filepath))
+                {
+                    var json = reader.ReadToEnd();
+                    var items = jsonConvert.DeserializeObject<List<Inventory>>(json);
+                    Console.WriteLine("Name\t" + "Price\t" + "Weight\t" + "Total amount");
+                    foreach (var data in items)
+                    {
+                        Console.WriteLine(data.Name + "\t" + data.Price + "\t" + data.Weight + "\t" + data.Price * data.Weight);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+
+}
---
+++ OOPS/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InventoryManagement inventoryManagement = new InventoryManagement();
+            inventoryManagement.ReadData(@"C:\Users\OMKAR BADE\source\repos\OOPS\OOPS\Inventory.json");
+            Console.ReadKey();
+        }
+    }
+}
